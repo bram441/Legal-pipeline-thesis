6ff8f9ce62b5a98700eebfabf71178afc9486093
Bram De Coster <bram.decoster1@gmail.com>
2026-03-01 12:48:02 +0100

    improved logging

--- pipeline/extraction/extractor.py
+++ pipeline/extraction/extractor.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+from debug import status_log
 from pipeline.extraction.openai_extractor import (
     extract_case_only_openai,
     extract_query_only_openai,
@@ -56,7 +57,11 @@ def extract_case_and_query(case_text, user_question, kb_schema=None, provider="a
     case_feedback = None
     case_obj = None
     last_case_error = None
-    for _ in range(max_retries):
+    for case_attempt in range(max_retries):
+        if case_attempt == 0:
+            status_log("Case", "Extracting")
+        else:
+            status_log("Case", "Repair attempt {}".format(case_attempt))
         try:
             case_obj = extract_case_only_openai(
                 case_text,
@@ -68,6 +73,7 @@ def extract_case_and_query(case_text, user_question, kb_schema=None, provider="a
             raise ExtractionError(str(e))
 
         try:
+            status_log("Case", "Validating")
             case = normalize_and_validate_case(case_obj, kb_schema=kb_schema)
             break
         except ValueError as e:
@@ -80,7 +86,11 @@ def extract_case_and_query(case_text, user_question, kb_schema=None, provider="a
     query_feedback = None
     query_obj = None
     last_query_error = None
-    for _ in range(max_retries):
+    for query_attempt in range(max_retries):
+        if query_attempt == 0:
+            status_log("Query", "Extracting")
+        else:
+            status_log("Query", "Repair attempt {}".format(query_attempt))
         try:
             query_obj = extract_query_only_openai(
                 user_question,
@@ -92,6 +102,7 @@ def extract_case_and_query(case_text, user_question, kb_schema=None, provider="a
             raise ExtractionError(str(e))
 
         try:
+            status_log("Query", "Validating")
             query = normalize_and_validate_query(query_obj, case, kb_schema=kb_schema)
             break
         except ValueError as e:
